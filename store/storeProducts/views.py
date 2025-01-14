0ef77a0961222ac3b3d1e07c20fcac0d9b26e734
Yermekov Dias <diasermekov09@gmail.com>
2025-01-15 00:04:44 +0500

    2nd commit

--- store/storeProducts/views.py
+++ store/storeProducts/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'storeProducts/index.html')
+    context = {
+        'title': 'StoreApp',
+        'username': 'Dias Yermekov',
+        'is_promotion': False
+    }
+    return render(request, 'storeProducts/index.html', context)
+
+def products(request):
+    context = {
+        'title': 'products'
+    }
+    return render(request, 'storeProducts/products.html', context)
